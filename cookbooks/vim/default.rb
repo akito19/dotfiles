1fd4720403a25498d845400c3c20ced1686bb141
Akito Kasai <a.kasai415@gmail.com>
2016-11-14 19:09:43 +0900

    removed extra option

--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -12,10 +12,8 @@ end
 # http://qiita.com/okamos/items/2259d5c770d51b88d75b
 execute "symlink .vim dir to nvim dir" do
   command "ln -snfv #{ENV['HOME']}/.vim #{ENV['HOME']}/.config/nvim/"
-  not_if "test -d #{ENV['HOME']}/.config/nvim/"
 end
 
 execute "symlink .vimrc to init.vim" do
   command "ln -snfv #{ENV['HOME']}/.vimrc #{ENV['HOME']}/.config/nvim/init.vim"
-  not_if "test -e #{ENV['HOME']}/.config/nvim/init.vim"
 end
