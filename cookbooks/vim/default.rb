fe25ceea1ba8f10843d773c5acb8fd554d0bc945
Akito Kasai <a.kasai415@gmail.com>
2016-11-15 15:40:43 +0900

    replaced execute resource with link

--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -10,10 +10,12 @@ directory "#{ENV['HOME']}/.config/nvim" do
 end
 
 # http://qiita.com/okamos/items/2259d5c770d51b88d75b
-execute "symlink .vim dir to nvim dir" do
-  command "ln -snfv #{ENV['HOME']}/.vim #{ENV['HOME']}/.config/nvim/"
+link "#{ENV['HOME']}/.vim" do
+  to "#{ENV['HOME']}/.config/nvim"
+  force true
 end
 
-execute "symlink .vimrc to init.vim" do
-  command "ln -snfv #{ENV['HOME']}/.vimrc #{ENV['HOME']}/.config/nvim/init.vim"
+link "#{ENV['HOME']}/.vimrc" do
+  to "#{ENV['HOME']}/.config/nvim/init.vim"
+  force true
 end
