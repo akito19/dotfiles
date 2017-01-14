5c7e4899d4c67dcc8e518f91b3f21106059cab61
Akito Kasai <a.kasai415@gmail.com>
2017-01-14 15:17:44 +0900

    add ghq setting to .gitconfig and make golang installed

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -1,12 +1,11 @@
 include_cookbook 'symboliclinks'
-include_cookbook 'vim'
 include_cookbook 'git'
+include_cookbook 'vim'
 include_cookbook 'zsh'
 include_cookbook 'ruby'
+include_cookbook 'golang'
 
 ln '.vimperatorrc'
 ln '.tmux.conf'
 ln '.bash_profile'
 ln 'bin'
-# TODO: peco を自動インストールするようにしてから
-# ln 'peco'
