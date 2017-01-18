58512a0c528302cbee3caa352cac583fdffc5e58
Akito Kasai <a.kasai415@gmail.com>
2017-01-18 14:56:30 +0900

    transfered #ask to bootstrap.rb

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -1,12 +1,20 @@
-include_cookbook 'base_darwin'
-include_cookbook 'symboliclinks'
-ln '.bash_profile'
+if ask('set up Xcode and Homebrew')
+  include_cookbook 'base_darwin'
+end
 
+include_cookbook 'symboliclinks'
 include_cookbook 'git'
 include_cookbook 'vim'
 include_cookbook 'zsh'
-include_cookbook 'ruby'
-include_cookbook 'golang'
 
+if ask('install ruby')
+  include_cookbook 'ruby'
+end
+
+if ask('install golang')
+  include_cookbook 'golang'
+end
+
+ln '.bash_profile'
 ln '.vimperatorrc'
 ln 'bin'
