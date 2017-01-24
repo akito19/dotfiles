c8d7aa07fc013847f86ce126dc92654c84ab09dd
Akito Kasai <a.kasai415@gmail.com>
2017-01-25 00:07:19 +0900

    changed the order of invoked cookbooks

--- roles/linux/default.rb
+++ roles/linux/default.rb
@@ -1,11 +1,17 @@
 include_cookbook 'symboliclinks'
 include_cookbook 'git'
 include_cookbook 'vim'
-include_cookbook 'zsh'
-include_cookbook 'tmux'
-include_cookbook 'ruby'
-include_cookbook 'golang'
+
+if ask 'install Ruby'
+  include_cookbook 'ruby'
+end
+
+if ask 'install Golang'
+  include_cookbook 'golang'
+end
 
 ln '.vimperatorrc'
 ln '.bash_profile'
-ln 'bin'
+
+include_cookbook 'tmux'
+include_cookbook 'zsh'
