87eb0365857660d356d2a6c1a6ac032c52ac6111
Akito Kasai <a.kasai415@gmail.com>
2017-04-06 17:30:56 +0900

    Commented out a part of zsh cookbook

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -6,16 +6,16 @@ git "#{zplug_home}" do
   not_if "test -d #{zplug_home}"
 end
 
-if node[:platform] == 'darwin'
-  execute 'change login-shell to Zsh' do
-    command 'chsh -s /bin/zsh'
-    not_if 'echo $SHELL | grep zsh'
-  end
-end
+# if node[:platform] == 'darwin'
+#   execute 'change login-shell to Zsh' do
+#     command 'chsh -s /bin/zsh'
+#     not_if 'echo $SHELL | grep zsh'
+#   end
+# end
 
 ln '.zshrc'
 
-execute 'exec zsh' do
-  command 'exec zsh'
-  not_if 'echo $SHELL | grep zsh'
-end
+# execute 'exec zsh' do
+#   command 'exec zsh'
+#   not_if 'echo $SHELL | grep zsh'
+# end
