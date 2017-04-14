cddaae6452bd946c5143c233b7ba98ab39905fbc
Akito Kasai <a.kasai415@gmail.com>
2017-04-14 17:52:32 +0900

    Add ptmux

--- cookbooks/golang/default.rb
+++ cookbooks/golang/default.rb
@@ -1,13 +1,13 @@
 package 'go'
 package 'peco'
 
-# make directory for golang
+# Make directory for golang
 directory "#{ENV['HOME']}/go/src/github.com/akito19" do
   action :create
   not_if "test -d #{ENV['HOME']}/go/src/github.com/akito19"
 end
 
-# install and make directory for ghq
+# Install and make directory for ghq
 execute "go get ghq" do
   command "go get github.com/motemen/ghq"
   not_if "test -d #{ENV['HOME']}/go/src/github.com/motemen/ghq"
@@ -21,6 +21,16 @@ end
 execute "go get hub" do
   command "go get github.com/github/hub"
   not_if "test -d #{ENV['HOME']}/go/src/github.com/github/hub"
+end
+
+execute "go get ptmux" do
+  command "go get github.com/pocke/ptmux"
+  not_if "test -d #{ENV['HOME']}/go/src/github.com/pocke/ptmux"
+end
+
+directory "#{ENV['HOME']}/.config/ptmux" do
+  action :create
+  not_if "test -d #{ENV['HOME']}/.config/ptmux"
 end
 
 ln '.peco'
