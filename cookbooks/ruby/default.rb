c8ee15dd3146e91e5751e0def2bb231072745dee
Akito Kasai <kasai@akito19.com>
2022-07-26 18:37:23 +0900

    Setup rbenv only

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -1,15 +1,26 @@
-node.reverse_merge!(
-  rbenv: {
-    user: 'akito',
-    global: '3.0.2',
-    versions: %w[
-      3.0.2
-      2.7.4
-    ],
-  }
-)
+rbenv_root = "#{ENV['HOME']}/.rbenv"
+git "#{rbenv_root}" do
+  repository "https://github.com/rbenv/rbenv.git"
+  not_if "test -d #{rbenv_root}"
+end
 
-include_recipe "rbenv::user"
+execute "make rbenv" do
+  command "cd #{rbenv_root} && src/configure && make -C src "
+end
+
+directory  "#{rbenv_root}/plugins" do
+  action :create
+  not_if "test -d #{rbenv_root}/plugins"
+end
+
+plugin_dir = "#{rbenv_root}/plugins/ruby-build"
+git "#{plugin_dir}" do
+  repository "https://github.com/rbenv/ruby-build.git"
+end
+
+execute "rbenv doctor" do
+  command "curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | bash"
+end
 
 execute "bundler settings" do
   command "bundle config set path 'vendor/bundle'"
