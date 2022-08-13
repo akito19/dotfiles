2463a5ed1c8c07e7230cd8328ff79a0950505f2c
Akito Kasai <kasai@akito19.com>
2022-08-13 23:39:13 +0900

    Update Rust

--- cookbooks/rust/default.rb
+++ cookbooks/rust/default.rb
@@ -2,13 +2,15 @@ rustc_path = "#{ENV['HOME']}/.cargo/bin/rustc"
 
 if node[:platform] == 'darwin'
   execute 'install Rust' do
-    command 'curl https://sh.rustup.rs -sSf | sh'
+    command "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
     not_if "test -e #{rustc_path}"
   end
 else
   # TODO
   MItamae.logger.info("It hasn't set recipe...")
 end
+
+package 'ripgrep'
 
 # Need for alacritty(Copy & Paste)
 package 'xclip'
